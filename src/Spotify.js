ff3e4f63bdeb7e7931206cf90f89b0a447cab79a
martins-wescley <wescleymartins02@gmail.com>
2018-05-30 14:27:40 -0300

    AlbumLists done

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-wrapper';
 
 const spotify = new SpotifyWrapper({
-  token: 'foo'
+  token: 'BQAweP_f3e6bW4UtsN9a_oiSbe-uzxs7T1FgfR3GouHM3eHEJ28Q9n4JHnj-iQrE1-wv094Kj6vyvnYWFt3xuDh6Cjjrqz0RCKAsHy1rkMM6lCn1cfFIRamCnCbujoPW7aHs3rMOhRpVKfg9ZTkAJvBgyuygcg'
 });
 
 export default spotify;
